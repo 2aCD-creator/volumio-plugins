cc7672086901fa38ab091797a088db7c30568635
2aCD <87193153+2aCD-creator@users.noreply.github.com>
2021-08-26 21:06:16 +0200

    Delete plugins/miscellanea/peppy_screensaver directory

--- plugins/miscellanea/peppy_screensaver/restoreAsound.js
+++
@@ -1,9 +0,0 @@
-const io = require('socket.io-client');
-const socket = io.connect('http://localhost:3000');
-
-// Call the endpoint
-const restoreAsoundConfig = { 'endpoint': 'miscellanea/peppy_screensaver', 'method': 'restoreAsoundConfig', 'data': '' };
-socket.emit('callMethod', restoreAsoundConfig);
-
-// Sleep and exit as I don't feel like figuring out the correct push response.
-setTimeout(() => process.exit(0), 1000);
