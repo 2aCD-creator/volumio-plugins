309c45126d150b3bd7ad2d9b33c7450ceee4cdd8
2aCDcreator <falk-seifert@arcor.de>
2021-10-09 21:31:43 +0000

    Removed folder from repository

--- plugins/miscellanea/peppy_screensaver/README.md
+++
@@ -1,5 +0,0 @@
-## PeppyScreensaver
-PeppyMeter as screensaver for Volumio on raspberry pi
->Many thanks to peppy.player, who provided the conditions for this
-
- 
--- plugins/miscellanea/peppy_screensaver/volumio_peppymeter/run_peppymeter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /data/plugins/miscellanea/peppy_screensaver/peppymeter
-
-if ! pgrep -x "python3" > /dev/null
-then
-	export DISPLAY=:0
-	python3 volumio_peppymeter.py
-fi
